0bc99cf4953459a5d39b12c476c6f752e9d4e292
myh473 <31515951+myh473@users.noreply.github.com>
2017-09-06 19:49:18 -0400

    added rand array

--- Homework 2/arrayDemo.php
+++ Homework 2/arrayDemo.php
@@ -12,8 +12,19 @@
 		$arraySize = $_POST['arraySize'];
 		$min = $_POST['min'];
 		$max = $_POST['max'];
+		$numArray = array();
 
-		print("<p> arraysize = $arraySize min = $min max = $max</p>");
+		print("<p> arraysize = $arraySize min = $min max = $max<br></p>");
+
+		for ($i = 0; $i < $arraySize; $i++) {
+			$numArray[$i] = rand($min ,$max);
+
+		}
+
+		for ($i = 0; $i < $arraySize; $i++) {
+			print("<p> Num $i = $numArray[$i]<br></p>");
+
+		}
 	?>
 	<p><input type = "submit" value = "give er a go!"></p>
 </body>
