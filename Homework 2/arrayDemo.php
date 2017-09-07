ad3d9a8fc317a8806c68bf74dd82de419ad5f0c1
myh473 <31515951+myh473@users.noreply.github.com>
2017-09-06 20:19:05 -0400

    don't let em do it!

--- Homework 2/arrayDemo.php
+++ Homework 2/arrayDemo.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 	<title>Array Demo</title>
+	<link rel ="stylesheet" type="text/css" href="sample.css">
 </head>
 
 <body>
@@ -14,7 +15,15 @@
 		$max = $_POST['max'];
 		$numArray = array();
 
+	if(is_numeric($min) && is_numeric($max) && is_numeric($arraySize) ) {
+		if($min < $max) {
 		print("<p> arraysize = $arraySize min = $min max = $max<br></p>");
+		}
+
+		else{
+			print("<p> arraysize = $arraySize min = $max max = $min<br></p>");
+		}
+
 
 		for ($i = 0; $i < $arraySize; $i++) {
 			$numArray[$i] = rand($min ,$max);
@@ -25,7 +34,11 @@
 			print("<p> Num $i = $numArray[$i]<br></p>");
 
 		}
+	}
+		else{
+	print("<h1>What ya doin there?</h1>");
+	}
 	?>
-	<p><input type = "submit" value = "give er a go!"></p>
+	<p><a href="arrayDemo.html">Click here to try again.</a></p>
 </body>
 </html>
