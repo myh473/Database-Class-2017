e9da718cf5e147a9981c0ea4459f94963fa6b108
myh473 <31515951+myh473@users.noreply.github.com>
2017-09-06 21:18:55 -0400

    push it

--- Homework 2/arrayDemo.php
+++ Homework 2/arrayDemo.php
@@ -14,8 +14,15 @@
 		$min = $_POST['min'];
 		$max = $_POST['max'];
 		$numArray = array();
+		$sqrtArray = array();
+		$squareArray = array();
+		$randUpArray = array();
+		$stringArray = array();
+		$areaArray = array();
+		$volArray = array();
 
-	if(is_numeric($min) && is_numeric($max) && is_numeric($arraySize) ) {
+
+	if(is_numeric($min) && is_numeric($max) && is_numeric($arraySize) && $arraySize > 0 ) {
 		if($min < $max) {
 		print("<p> arraysize = $arraySize min = $min max = $max<br></p>");
 		}
@@ -27,14 +34,31 @@
 
 		for ($i = 0; $i < $arraySize; $i++) {
 			$numArray[$i] = rand($min ,$max);
-
 		}
 
 		for ($i = 0; $i < $arraySize; $i++) {
+			$number = abs ($numArray[$i]);	
+			$sqrtArray[$i] = sqrt($number);
+		}
+
+		for ($i = 0; $i < $arraySize; $i++) {
+			$number = pow (squareArray[$i] , 2)
+			$squareArray[$i] = $number;
+		}
+
+
+
+
+
+				for ($i = 0; $i < $arraySize; $i++) {		//FOR TESTING
 			print("<p> Num $i = $numArray[$i]<br></p>");
+			echo "SquareRoot is " . number_format($sqrtArray[$i],3)."<br>";
+			echo "squared is " . number_format($squareArray[$i],3)."<br>";
 
 		}
-	}
+
+}
+
 		else{
 	print("<h1>What ya doin there?</h1>");
 	}
