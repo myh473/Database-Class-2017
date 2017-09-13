cf243c1cd3381827f16380ae98b937798eb96dd8
Blubberchub <31516045+Blubberchub@users.noreply.github.com>
2017-09-13 19:30:00 -0400

    Add files via upload

--- Homework3/wordGame.php
+++ Homework3/wordGame.php
@@ -11,12 +11,19 @@
 	<?php 
 		$letterAmount = $_POST['letterAmount'];
 		$randomLetters = array();
-
 	for($i = 0; $i < $letterAmount; $i++) {
 		$randomLetters[$i] = chr(rand(97,122));
 		print ("<tr> <td> $randomLetters[$i] </td> </tr>");
 	}
-
 	?>
-</body>
-</html>
+	<h2></h2>
+	<form action = "substrings.php" method = "post" >
+	<input type = "submit" value = "Substrings">
+	</form>
+	<form action = "permutations.php" method = "post" >
+	<input type = "submit" value = "Permutations">
+	</form>
+	<form action = "words.php" method = "post" >
+	<input type = "submit" value = "Possible Words">
+	</body>
+	</html>
