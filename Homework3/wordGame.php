dbd6da675f9b57123c35d0981a4144eafb8f26ac
myh473 <31515951+myh473@users.noreply.github.com>
2017-09-13 19:16:15 -0400

    random letters

--- Homework3/wordGame.php
+++ Homework3/wordGame.php
@@ -1,1 +1,22 @@
-dank
+<html>
+<head>
+	<title>Word Game</title>
+	<link rel ="stylesheet" type="text/css" href="sample.css">
+
+</head>
+
+<body>
+	<h1>Here are your random letter pal</h1>
+
+	<?php 
+		$letterAmount = $_POST['letterAmount'];
+		$randomLetters = array();
+
+	for($i = 0; $i < $letterAmount; $i++) {
+		$randomLetters[$i] = chr(rand(97,122));
+		print ("<tr> <td> $randomLetters[$i] </td> </tr>");
+	}
+
+	?>
+</body>
+</html>
