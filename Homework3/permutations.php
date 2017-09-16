d8a713d8c88cb800a8f549806b82570f230ecd0c
Blubberchub <31516045+Blubberchub@users.noreply.github.com>
2017-09-16 18:27:25 -0400

    Add files via upload

--- Homework3/permutations.php
+++ Homework3/permutations.php
@@ -1,1 +1,25 @@
-lit
+
+<link rel ="stylesheet" type="text/css" href="sample.css">
+	<?php
+print_r(AllPermutations(array('peter', 'paul', 'mary')));
+
+function AllPermutations($InArray, $InProcessedArray = array())
+{
+    $ReturnArray = array();
+    foreach($InArray as $Key=>$value)
+    {
+        $CopyArray = $InProcessedArray;
+        $CopyArray[$Key] = $value;
+        $TempArray = array_diff_key($InArray, $CopyArray);
+        if (count($TempArray) == 0)
+        {
+            $ReturnArray[] = $CopyArray;
+        }
+        else
+        {
+            $ReturnArray = array_merge($ReturnArray, AllPermutations($TempArray, $CopyArray));
+        }
+    }
+    return $ReturnArray;
+}
+	?>
