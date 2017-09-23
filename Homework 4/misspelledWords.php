00d800d05a458417efe89f58f0a14f6303eb6ca1
myh473 <31515951+myh473@users.noreply.github.com>
2017-09-23 03:03:23 -0400

    Misspelled words is finished
    Loops are your friend

--- Homework 4/misspelledWords.php
+++ Homework 4/misspelledWords.php
@@ -1,15 +1,17 @@
 <html>
 <head>
-	<title>Word Game</title>
+	<title>Misspelled Words</title>
 	<link rel ="stylesheet" type="text/css" href="sample.css">
 
 </head>
 
 <body>
-	<h1>Here are your random letter pal</h1>
+	<h1>Oh wow look at all these random words! I hope they are here, because I can't see them that's for sure, because of the ants. I'm ants in my eyes Johnson!</h1>
 
 	<?php 
 
+	$realWords = fopen("RealWords.txt", "w");
+	$fakeWords = fopen("FakeWords.txt", "w");
 	$lines = file("engmix.txt", FILE_IGNORE_NEW_LINES);
 	$randomWords = array();
 	$numWords = 1000;
@@ -35,10 +37,8 @@
 	for($j = 0; $j < sizeof($lines); $j++) { //taken from HW 3
 					if(isset($randomWords[$lines[$j]])) {
 						$wordsGood[] = $lines[$j];
-
-						//print("aw nutts");
-						//fwrite($wordsFile, $lines[$j]);
-						//fwrite($wordsFile, "\n");
+						fwrite($realWords, $lines[$j]);
+						fwrite($realWords, "\n");
 					}
 				}
 
@@ -47,26 +47,33 @@
 			print("</pre>");
 
 					foreach($randomWords as $value) {
+						$count = 0;
 						for($i = 0; $i < sizeof($wordsGood); $i++) {
 						//	print("$i is i $value is value<br>");
 							if($randomWords[$value] == $wordsGood[$i]) {
-								print("nutts<br>");	
-								break;
-							}
-							else {
-								$wordsBad[] = $randomWords[$value];
-								
-							}
+								$count = 1;
+							}//if
+						}//for
+						if($count == 0) {
+						$wordsBad[] = $randomWords[$value];
+						fwrite($fakeWords, $randomWords[$value]);
+						fwrite($fakeWords, "\n");
 						}
-					}
-
-
-
+					}//foreach
 
 			print("<pre>Here are things that are not words");
 			print_r($wordsBad);
 			print("</pre>");
 
+			asort($wordsBad);
+
+			print("<pre>Here are things that are not words -Sorted");
+			print_r($wordsBad);
+			print("</pre>");
+
+
+			fclose($realWords);
+			fclose($fakeWords);
 
 	?>
 
