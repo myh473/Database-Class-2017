1425442e2e29d7eff80161eb9f4170190d8894c1
myh473 <31515951+myh473@users.noreply.github.com>
2017-09-23 22:50:38 -0400

    Frequency is functional!

--- Homework 4/frequency.php
+++ Homework 4/frequency.php
@@ -13,38 +13,39 @@
 		$wordsFile = fopen("letter_frequency.txt", "w");
 		$words = file("engmix.txt", FILE_IGNORE_NEW_LINES);
 		$hugeWord = implode($words);
-		$letter_A = substr_count($hugeWord, 'a');
-		$letter_B = substr_count($hugeWord, 'b');
-		$letter_C = substr_count($hugeWord, 'c');
-		$letter_D = substr_count($hugeWord, 'd');
-		$letter_E = substr_count($hugeWord, 'e');
-		$letter_F = substr_count($hugeWord, 'f');
-		$letter_G = substr_count($hugeWord, 'g');
-		$letter_H = substr_count($hugeWord, 'h');
-		$letter_I = substr_count($hugeWord, 'i');
-		$letter_J = substr_count($hugeWord, 'j');
-		$letter_K = substr_count($hugeWord, 'k');
-		$letter_L = substr_count($hugeWord, 'l');
-		$letter_M = substr_count($hugeWord, 'm');
-		$letter_N = substr_count($hugeWord, 'n');
-		$letter_O = substr_count($hugeWord, 'o');
-		$letter_P = substr_count($hugeWord, 'p');
-		$letter_Q = substr_count($hugeWord, 'q');
-		$letter_R = substr_count($hugeWord, 'r');
-		$letter_S = substr_count($hugeWord, 's');
-		$letter_T = substr_count($hugeWord, 't');
-		$letter_U = substr_count($hugeWord, 'u');
-		$letter_V = substr_count($hugeWord, 'v');
-		$letter_W = substr_count($hugeWord, 'w');
-		$letter_X = substr_count($hugeWord, 'x');
-		$letter_Y = substr_count($hugeWord, 'y');
-		$letter_Z = substr_count($hugeWord, 'z');
-		$sum = $letter_A + $letter_B+ $letter_C + $letter_D + $letter_E + $letter_F + $letter_G + $letter_H + $letter_I + $letter_J + $letter_K + $letter_L + $letter_M + $letter_N + $letter_O + $letter_P + $letter_Q + $letter_R + $letter_S + $letter_T + $letter_U + $letter_V + $letter_W + $letter_X + $letter_Y + $letter_Z;
+		$total = 0;
+
+		$sum = 0;
 
 
-		print("A = $letter_A<br>");
-		print("B = $letter_B<br>");
-		print("sum = $sum<br>");
+
+
+		for($i = 0; $i < 26; $i++) {
+
+			$hold = chr($i + 97);
+			print("$hold<br>");
+			$letterHold[$hold] = substr_count($hugeWord, $hold);
+			print("letter hold is $letterHold[$hold]<br>");
+			$sum += $letterHold[$hold];
+		}
+
+			print("<pre>Letter Frequency ");
+			print_r($letterHold);
+			print("</pre>");
+
+			foreach($letterHold as $key => $value) {
+				$percentage[$key] = round($letterHold[$key] / ($sum / 100), 2); //there is a rounding error but otherwise works as intended.
+				$total += $percentage[$key];
+				$insert =("$key : $letterHold[$key] : $percentage[$key] \n");
+				fwrite($wordsFile, $insert);
+
+			}
+
+			print("<pre>Letter Frequency $total ");
+			print_r($percentage);
+			print("</pre>");
+
+
 
 
 	fclose($wordsFile);
