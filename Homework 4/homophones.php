2c2090da3429d58826209d6b40013652793d7790
myh473 <31515951+myh473@users.noreply.github.com>
2017-09-28 12:32:13 -0400

    Revert "Add files via upload"
    This reverts commit 8a49877cfc4792db3bca51e5a147d1c1e81274ff.

--- Homework 4/homophones.php
+++ Homework 4/homophones.php
@@ -9,8 +9,72 @@
 	<h1>Oh wow look at all these random words! I hope they are here, because I can't see them that's for sure, because of the ants. I'm ants in my eyes Johnson!</h1>
 
 	<?php 
+	$homophones = fopen("homophones.txt", "w");
+	$fakeWords = file("fakeWords.txt", FILE_IGNORE_NEW_LINES);
+	$words = file("engmix.txt", FILE_IGNORE_NEW_LINES);
+	$soundexArray = array();
+	$metaphoneArray = array();
+
+	for($j = 0; $j < sizeof($words); $j++) {
+			$soundexArray[] = soundex($words[$j]);
+			$metaphoneArray[] = metaphone($words[$j]);
+	}
 	
+	for($j = 0; $j < sizeof($fakeWords); $j++) {
+			$fakeSoundexArray[] = soundex($fakeWords[$j]);
+			$fakeMetaphoneArray[] = metaphone($fakeWords[$j]);
+	}
+
+	for($i = 0; $i < sizeof($fakeWords); $i++) {
+		for($j = 0; $j < sizeof($words); $j++) {
+			if($soundexArray[$j] == $fakeSoundexArray[$i]) {
+				$finalSoundexArray[$fakeWords[$i]][] = $words[$j];
+			}
+			if($metaphoneArray[$j] == $fakeMetaphoneArray[$i]) {
+				$finalMetaphoneArray[$fakeWords[$i]][] = $words[$j];
+			}
+		}
+	}
+			/*
+			print("<pre>Soundex ");
+			print_r($finalSoundexArray);
+			print("</pre>");
+
+			print("<pre>Metaphone ");
+			print_r($finalMetaphoneArray);
+			print("</pre>");
+			*/
+
+			for($i = 0; $i < sizeof($fakeWords); $i++) {
+				if(array_key_exists($fakeWords[$i], $finalSoundexArray)) {
+					$writeString = ("Soundex of $fakeWords[$i] : " . sizeof($finalSoundexArray[$fakeWords[$i]])) . " ";
+					for($j = 0; $j < sizeof($finalSoundexArray[$fakeWords[$i]]); $j++) {
+						$writeString .= " : " . $finalSoundexArray[$fakeWords[$i]][$j];
+					}
+					$writeString .= "\n";
+					fwrite($homophones, $writeString);
+				//	print("Soundex = $writeString<br>");
+				}
+
+				if(array_key_exists($fakeWords[$i], $finalMetaphoneArray)) {
+					$writeString = ("Metaphone of $fakeWords[$i] : " . sizeof($finalMetaphoneArray[$fakeWords[$i]])) . " ";
+					for($j = 0; $j < sizeof($finalMetaphoneArray[$fakeWords[$i]]); $j++) {
+						$writeString .= " : " . $finalMetaphoneArray[$fakeWords[$i]][$j];
+					}
+					$writeString .= "\n";
+					fwrite($homophones, $writeString);
+				//	print("Metaphone = $writeString<br>");
+				}
+
+			}
+
+
+
+	fclose($homophones);
 	?>
+	<form action = "start.html" method = "post" >
+    <input type = "submit" value = "Back to the start">
+    </form>
 	<form action = "analysis.php" method = "post" >
 	<input type = "submit" value = "Analysis">
 	</form>
@@ -26,8 +90,7 @@
 	<form action = "levenshtein.php" method = "post" >
 	<input type = "submit" value = "Levenshtein">
 	</form>	
-     <form action = "start.html" method = "post" >
-    <input type = "submit" value = "Back to the start">
-    </form>
+	<form action = "logout.php" method = "post" >
+	<input type = "submit" value = "Start Over">
 </body>
 </html>
