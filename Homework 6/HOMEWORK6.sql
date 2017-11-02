b593828538aad92a11573d65c5431d41483b2d83
Blubberchub <31516045+Blubberchub@users.noreply.github.com>
2017-11-01 22:44:42 -0400

    Add files via upload

--- Homework 6/HOMEWORK6.sql
+++ Homework 6/HOMEWORK6.sql
@@ -1,3 +1,6 @@
-SELECT f.title, a.first_name, a.last_name FROM film f, actor a, film_actor fa WHERE f.rating = "G" AND f.film_id = fa.film_id AND a.actor_id = fa.actor_id;
+SELECT * from film;
+SELECT * from category;
+SELECT * from film_category;
 
 SELECT f.title, f.rental_rate, l.name FROM film f, category c, film_category fc, language l WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND c.name = "Sci-Fi" AND f.language_id = l.language_id;
+SELECT f.title FROM film f, category c, film_category fc WHERE f.film_id = fc.film_id AND fc.category_id AND fc.category_id = c.category_id AND c.name = "Action" AND (f.rating = "PG-13" OR f.rating = "R" OR "NC-17");
