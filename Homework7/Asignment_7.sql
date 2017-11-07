6c5f13283b33ef59cad340bd649f0446784272e6
myh473 <31515951+myh473@users.noreply.github.com>
2017-11-06 20:44:46 -0500

    doobies
    are bad

--- Homework7/Asignment_7.sql
+++ Homework7/Asignment_7.sql
@@ -1,5 +1,5 @@
-SELECT * FROM film_actor;
-SELECT * FROM customer;
+SELECT * FROM film;
+SELECT * FROM category;
 SELECT * FROM payment;
 SELECT * FROM rental;
 SELECT * FROM actor;
@@ -9,3 +9,5 @@ SELECT * FROM city;
 
 
 SELECT customer.first_name, customer.last_name, address.address, address.district, address.postal_code FROM customer LEFT JOIN address ON  address.address_id = customer.address_id;
+
+SELECT film.title, film.description, category.name FROM film INNER JOIN film_category ON film.film_id = film_category.film_id INNER JOIN category ON film_category.category_id = category.category_id;
