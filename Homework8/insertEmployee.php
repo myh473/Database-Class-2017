40b285643bca31ab85194f9fbb74d4bbbe69d658
myh473 <31515951+myh473@users.noreply.github.com>
2017-11-13 19:58:32 -0500

    butts

--- Homework8/insertEmployee.php
+++ Homework8/insertEmployee.php
@@ -1,1 +1,25 @@
-and how
+<html>
+<head>
+	<title>Insert Employee</title>
+	<link rel ="stylesheet" type ="text/css" herf="sample.css">
+
+</head>
+
+<body>
+
+
+	<?php
+
+
+
+	//lotsa sql here
+
+
+	?>
+
+
+
+
+
+</body>
+</html>
--- Homework8/view_emps.php
+++ Homework8/view_emps.php
@@ -1,1 +1,28 @@
-and how
+<html>
+<head>
+	<title>Add Employee</title>
+	<link rel ="stylesheet" type ="text/css" herf="sample.css">
+
+</head>
+
+<body>
+
+
+	<?php
+
+
+
+	//lotsa sql here
+
+
+	?>
+
+	<form action = "manager.html" method = "post" >
+    <input type = "submit" value = "To manager">
+    </form>
+
+
+
+
+</body>
+</html>
