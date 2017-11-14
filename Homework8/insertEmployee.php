73d626e220f6278e1e46d4a3437ade227a2d2cf7
Blubberchub <31516045+Blubberchub@users.noreply.github.com>
2017-11-14 12:24:30 -0500

    Add files via upload

--- Homework8/insertEmployee.php
+++ Homework8/insertEmployee.php
@@ -9,26 +9,18 @@
 
 
 	<?php
-
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$dbName = "sakila";
-
 	$conn = new mysqli($servername, $username, $password, $dbName);
-
 	if ($conn->connect_error) {
 		die("Connection failed" . $conn->connect_error);
 	}
-
 	echo "Connected successfully<br>";
-
-
 	$sql = "SELECT s.last_name, s.first_name, a.address, c.city, a.district, co.country, a.postal_code, s.email, a.phone FROM staff s, address a, city c, country co WHERE s.address_id = a.address_id AND a.city_id = c.city_id AND c.country_id = co.country_id;";
 	$result = $conn->query($sql);
-
 	if (mysqli_num_rows($result) > 0) {
-
 		print("<table border = \"2\"> <tr>
 			<th>Last Name</th>
 			<th>First Name</th>
@@ -40,10 +32,7 @@
 			<th>Email Address</th>
 			<th>Phone Number</th>
 		</tr>");
-
 		while($row = mysqli_fetch_assoc($result)) {
-
-
 			$a = $row["last_name"];
 			$b = $row["first_name"];
 			$c = $row["address"];
@@ -53,8 +42,6 @@
 			$g = $row["postal_code"];
 			$h = $row["email"];
 			$i = $row["phone"];
-
-
 			print("<tr> <td>$a</td>
 				<td class = \"center\">$b</td>
 				<td class = \"center\">$c</td>
@@ -65,16 +52,12 @@
 				<td class = \"center\">$h</td>
 				<td class = \"center\">$i</td>
 				</tr>");
-
 		}
 		print("</table>");
 	}
 	else {
 		echo "No results";
 	}
-
-
-
 	?>
 
 
--- Homework8/view_emps.php
+++ Homework8/view_emps.php
@@ -13,21 +13,14 @@
 	$username = "root";
 	$password = "";
 	$dbName = "sakila";
-
 	$conn = new mysqli($servername, $username, $password, $dbName);
-
 	if ($conn->connect_error) {
 		die("Connection failed" . $conn->connect_error);
 	}
-
 	echo "Connected successfully<br>";
-
-
 	$sql = "SELECT s.last_name, s.first_name, a.address, c.city, a.district, co.country, a.postal_code, s.email, a.phone FROM staff s, address a, city c, country co WHERE s.address_id = a.address_id AND a.city_id = c.city_id AND c.country_id = co.country_id;";
 	$result = $conn->query($sql);
-
 	if (mysqli_num_rows($result) > 0) {
-
 		print("<table border = \"2\"> <tr>
 			<th>Last Name</th>
 			<th>First Name</th>
@@ -39,10 +32,7 @@
 			<th>Email Address</th>
 			<th>Phone Number</th>
 		</tr>");
-
 		while($row = mysqli_fetch_assoc($result)) {
-
-
 			$a = $row["last_name"];
 			$b = $row["first_name"];
 			$c = $row["address"];
@@ -52,8 +42,6 @@
 			$g = $row["postal_code"];
 			$h = $row["email"];
 			$i = $row["phone"];
-
-
 			print("<tr> <td>$a</td>
 				<td class = \"center\">$b</td>
 				<td class = \"center\">$c</td>
@@ -64,16 +52,12 @@
 				<td class = \"center\">$h</td>
 				<td class = \"center\">$i</td>
 				</tr>");
-
 		}
 		print("</table>");
 	}
 	else {
 		echo "No results";
 	}
-
-
-
 	?>
 
 	<form action = "manager.html" method = "post" >
